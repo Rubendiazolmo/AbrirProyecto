b5ab156c17c3531bcc487c5bad690e764142d9d1
Rubendiazolmo <86986250+Rubendiazolmo@users.noreply.github.com>
2022-02-19 19:49:20 +0100

    Update AbriProyecto.bat

--- AbriProyecto.bat
+++ AbriProyecto.bat
@@ -1,9 +1,9 @@
 @echo off
 
 set /p id="Enter ID: "
-set ruta="D:\..Borrar\batch_csv\prueba\"
+set ruta_busqueda="D:\..Borrar\batch_csv\prueba\"
 
-FOR /F "tokens=* USEBACKQ" %%F IN (`%ruta%*%id%* /AD /b /s`) DO (
+FOR /F "tokens=* USEBACKQ" %%F IN (`dir %ruta_busqueda%*%id%* /AD /b /s`) DO (
 SET ruta=%%F
 )
 explorer %ruta%
